f62fe205b689fcd29c8599303705b085991f9511
gogoglrind <gogolgrind@gmail.com>
2025-12-13 18:46:35 +0300

    No tuples, because of optuna

--- universal_validator/tasks/classification_task.py
+++ universal_validator/tasks/classification_task.py
@@ -40,7 +40,7 @@ class ClassificationTask(BaseTask):
                 'min_samples_leaf': [1, 2, 4],
             },
             'mlp': {
-                'hidden_layer_sizes': [(64,), (128,), (64, 64), (128, 64)],
+                'hidden_layer_sizes': [64, 128],
                 'alpha': [0.0001, 0.001, 0.01, 0.1],
                 'learning_rate_init': [0.001, 0.01, 0.1],
             },
