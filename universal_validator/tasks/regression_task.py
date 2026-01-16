c2b2e2271729a2e9e351189f0f68735a580a547b
xxraytz <egorsurkov.ds@gmail.com>
2026-01-16 13:57:42 +0000

    Add new datasets preprocessing. Turn off random forest.

--- universal_validator/tasks/regression_task.py
+++ universal_validator/tasks/regression_task.py
@@ -23,7 +23,7 @@ class RegressionTask(BaseTask):
         
         # Define models including CatBoost
         self.models = {
-            'random_forest': RandomForestRegressor(n_estimators=100, random_state=42),
+            # 'random_forest': RandomForestRegressor(n_estimators=100, random_state=42),
             'mlp': MLPRegressor(hidden_layer_sizes=(100, 50), random_state=42, max_iter=1000),
             'catboost': CatBoostRegressor(
                 iterations=100,
