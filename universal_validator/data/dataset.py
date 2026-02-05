eea0cac4dd1c01e7055a6b9fae8031d5d96cce35
xaosina <dima.tina2013@gmail.com>
2026-02-05 11:59:33 +0000

    Cleanup

--- universal_validator/data/dataset.py
+++ universal_validator/data/dataset.py
@@ -16,20 +16,21 @@ class ValidatorDataset:
     def get_available_tasks(self, verbose=False):
         cols = pq.ParquetDataset(self.data_conf.train_path).schema.names
         target_cols = [col for col in cols if col.startswith("target__")]
-        if verbose:
-            print(f"Available tasks for {self.data_conf.dataset_name} dataset:")
+        if not verbose:
+            return target_cols
+
+        print(f"Available tasks for {self.data_conf.dataset_name} dataset:")
         for col in target_cols:
-            name, target_type, metric = self._parse_target_name(col)
-            if verbose:
-                print(
-                    f"Task Name: {name},\t{target_type} type,\tMetric: {metric}\t| {col}"
-                )
+            name, target_type, metrics = self._parse_target_name(col)
+            print(
+                f"Task Name: {name},\t{target_type} type,\tMetrics: {metrics}\t| {col}"
+            )
         return target_cols
 
     def load_for_task(self, target_name) -> Tuple[pd.DataFrame, pd.DataFrame]:
         print(f"Loading {self.data_conf.dataset_name} dataset...")
 
-        _, target_type, metric = self._parse_target_name(target_name)
+        _, target_type, metrics = self._parse_target_name(target_name)
         if target_type in ["local"]:
             columns = ["embeddings", target_name]
             train = pd.read_parquet(self.data_conf.train_path, columns=columns)
@@ -47,13 +48,14 @@ class ValidatorDataset:
             "y_train": y_train,
             "X_test": X_test,
             "y_test": y_test,
-            "metric": metric,
+            "metric": metrics,
         }
 
     def _parse_target_name(self, target_name):
         parts = target_name.split("__")
         assert parts[0] == "target"
         assert len(parts) == 4, parts
-        name, target_type, metric = parts[1:]
+        name, target_type, metrics = parts[1:]
         assert target_type in ["global", "local"], f"{target_type} is a wrong task type"
-        return name, target_type, metric
+        metrics = metrics.split("+")
+        return name, target_type, metrics
